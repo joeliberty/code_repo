b5764a1a9ec9a979a2eec04ba7b49e6728dce78a
joeliberty <joe@codewest.net>
2013-03-21 21:41:49 -0700

    Adding initial first_app project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6bb70767712c3264aa5cb15399700db456077b4c01378920632703445003218f2d54a5533c0001c6276a6bf97a484888766f47b7040cf39cf43c0cf12547bfd1'
